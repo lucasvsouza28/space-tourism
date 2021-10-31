11a14332a964b8937150c4f03847329d36d26d36
Lucas Souza <lucasvsouza28@gmail.com>
2021-10-31 00:07:03 -0300

    feat: menu mobile finished

--- src/common/components/Header/styles.ts
+++ src/common/components/Header/styles.ts
@@ -4,13 +4,16 @@ export const Container = styled.div`
     display: flex;
     justify-content: flex-end;
     align-items: center;
-
     height: 70px;
+    padding-right: 24px;
+
+    @media (min-width: 767px) {
+        padding: 0;
+    }
 `;
 
 export const HeaderNav = styled.nav`
-    height: 100%;
-    display: flex;
+    height: 100%;    
     align-items: center;
     justify-content: center;
     
@@ -19,6 +22,8 @@ export const HeaderNav = styled.nav`
     margin-top: 40px;
     gap: 37px;
     padding: 38px 46px;
+
+    display: none;
 
     a {
         font-family: 'Barlow Condensed', serif;
@@ -41,19 +46,38 @@ export const HeaderNav = styled.nav`
         }
     }
 
+    @media (min-width: 767px) {
+        display: flex;
+    }
+
     @media (min-width: 1024px) {
         gap: 82px;
         padding: 38px 120px;
     }
 `;
 
-export const Blur = styled.div`
+type BlurProps = {
+    active: boolean;
+};
+
+export const Blur = styled.div<BlurProps>`
     position: absolute;
     background: rgba(255, 255, 255, 0.04);
     backdrop-filter: blur(10.5485px);
-    width: 100%;
-    height: 100%;
+    height: 101vh;
+    width: 254px;
     z-index: 1;
+    right: 0px;
+    top: 0;
+    display: ${ props => props.active ? 'flex' : 'none' };
+    flex-direction: column;
+
+    @media(min-width: 767px) {
+        height: 100%;
+        width: 100%;
+        right: initial;
+        top: initial;
+    }
 `;
 
 export const Number = styled.div`
@@ -61,10 +85,10 @@ export const Number = styled.div`
     font-weight: 700;
     font-size: 16px;
     line-height: 19px;
-    display: none;
+    display: block;
 
-    @media (min-width: 1024px) {
-        display: block;            
+    @media (min-width: 767px) and (max-width: 1023px) {
+        display: none;
     }
 `;
 
@@ -83,4 +107,50 @@ export const BottomBar = styled.span`
     background-color: #FFF;
     position: absolute;
     bottom: 0;
+`;
+
+export const HeaderNavMobile = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+    z-index: 2;
+        
+    a {
+        font-family: Barlow Condensed;
+        text-decoration: none;
+        color: #FFF;
+
+        display: flex;
+        height: 19px;
+        align-items: center;
+        
+        &.active span{
+            display: block;
+        }
+
+        &:hover span {
+            display: block;
+            opacity: 0.5;
+        }
+    }
+
+
+    @media(min-width: 767px) {
+        display: none;
+    }
+
+`;
+
+export const CloseButtonContainer = styled.div`
+    display: flex;
+    justify-content: end;
+    padding: 1rem 1rem;
+    margin-bottom: 65px;
+`;
+
+export const NavLinkContainer = styled.div`
+    padding: 0 32px;
+    display: flex;
+    gap: 32px;
+    flex-direction: column;
 `;
